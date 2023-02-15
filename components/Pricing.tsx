34dfbc5ffc094dc5701ad4bcc924dad0d216f1e5
Johan Eliasson <johan@eliasson.me>
2023-02-15 15:14:35 +0100

    adding access token

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -39,7 +39,8 @@ export default function Pricing({ plans, userWithSubscription }: PricingProps) {
         {
           method: 'POST',
           headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${nhost.auth.getAccessToken()}`
           },
           body: JSON.stringify({
             priceId
